87f683b6417b7f9586e4da65c78f0489e3a1e243
Luis Wirth <lwirth2000@gmail.com>
2024-09-05 18:33:52 +0200

    405 current  2024-09-05 18:33:49  24.11.20240904.ad416d0  6.10.7                          *

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -2,6 +2,8 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.front_end="WebGpu"
+config.enable_wayland = false
+
 config.enable_tab_bar = false
 config.color_scheme = 'Harmonic16 Dark (base16)'
 
